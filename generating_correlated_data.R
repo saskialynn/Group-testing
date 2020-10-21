150db1cbba9bac1084600fdc905f5e5a086009d0
Saskia <saskia.comess@yale.edu>
2020-10-21 17:45:09 -0400

    update to generating correlated data
    tried to implement fxn in parallel, not working yet though

--- generating_correlated_data.R
+++ generating_correlated_data.R
@@ -1,52 +1,95 @@
 
 # Objective: generate data positively correlated with real data
-# https://stats.stackexchange.com/questions/15011/generate-a-random-variable-with-a-defined-correlation-to-an-existing-variables
 
 ####### A simple example #########
-n     <- 20                    # length of vector
-rho   <- 0.6                   # desired correlation = cos(angle)
-theta <- acos(rho)             # corresponding angle
-x1    <- rnorm(n, 1, 1)        # fixed given data
-x2    <- rnorm(n, 2, 0.5)      # new random data
-X     <- cbind(x1, x2)         # matrix
-Xctr  <- scale(X, center=TRUE, scale=FALSE)   # centered columns (mean 0)
-
-Id   <- diag(n)                               # identity matrix
-Q    <- qr.Q(qr(Xctr[ , 1, drop=FALSE]))      # QR-decomposition, just matrix Q (original data)
-P    <- tcrossprod(Q)          # = Q Q'       # projection onto space defined by x1
-x2o  <- (Id-P) %*% Xctr[ , 2]                 # x2ctr made orthogonal to x1ctr (generated correlated data)
-Xc2  <- cbind(Xctr[ , 1], x2o)                # bind to matrix
-Y    <- Xc2 %*% diag(1/sqrt(colSums(Xc2^2)))  # scale columns to length 1
-
-x <- Y[ , 2] + (1 / tan(theta)) * Y[ , 1]     # final new vector
-cor(x1, x)                                    # check correlation = rho
-
+# https://stats.stackexchange.com/questions/15011/generate-a-random-variable-with-a-defined-correlation-to-an-existing-variables
 
 ######### Generating Correlated Data for Pooled Sampling #########
 # Import Ct value info
 probit_input <- read.csv("probit_zscores_cts_tissue_agnostic.csv")
-probit_input <- probit_input[c(1:10000),2] # limit to just ct_values
+
+# limit to just ct_values and first 10,000 rows of data 
+# using more data produces "error: vector memory exhausted (limit reached)"
+probit_input <- probit_input[c(1:10000),2] 
+
 n <- length(probit_input)
-rho <- 0.2 # desired correlation (= cos(angle))
-theta <- acos(rho) # corresponding angle
-cor_dat <- rnorm(n, 1, 0.5) # new random data
+rho <- 0.2                                  # desired correlation (= cos(angle))
+theta <- acos(rho)                                         # corresponding angle
+cor_dat <- rnorm(n, 1, 0.5)                                    # new random data
 # should this be Weibull dis as well? which parameters to choose?
 
 X <- cbind(probit_input, cor_dat) #matrix
-Xctr <- scale(X, center = TRUE, scale = FALSE) # centered columns (mean = 0)
-rm(X)
-rm(probit_input) # remove unneeded data to save space
+Xctr <- scale(X, center = TRUE, scale = FALSE)     # centered columns (mean = 0)
+rm(X)                                       # remove unneeded data to save space
 
-Id <- diag(n) # identity matrix
-Q <- qr.Q(qr(Xctr[ , 1, drop = FALSE])) 
-# compute QR decomposition of Xctr (select column of original data)
-P    <- tcrossprod(Q)          # = Q Q'       # projection onto space defined by x1
-x2o  <- (Id-P) %*% Xctr[ , 2]                 # x2ctr made orthogonal to x1ctr (generated correlated data)
-Xc2  <- cbind(Xctr[ , 1], x2o)                # bind to matrix
-Y    <- Xc2 %*% diag(1/sqrt(colSums(Xc2^2)))  # scale columns to length 1
+Id <- diag(n)                                                # identity matrix
+Q <- qr.Q(qr(Xctr[ , 1, drop = FALSE]))     # QR decomp. of Xctr (original data)
+P <- tcrossprod(Q)             # = Q Q'    # projection onto space defined by x1
+x2o <- (Id-P) %*% Xctr[ , 2]                    # x2ctr made orthogonal to x1ctr 
+Xc2 <- cbind(Xctr[ , 1], x2o)                                   # bind to matrix
+Y <- Xc2 %*% diag(1/sqrt(colSums(Xc2^2)))            # scale columns to length 1
 
-x <- Y[ , 2] + (1 / tan(theta)) * Y[ , 1]     # final new vector
-cor(x1, x)                                    # check correlation = rho
+x <- Y[ , 2] + (1 / tan(theta)) * Y[ , 1]      # final vector of correlated data
+cor(probit_input, x)                                   # check correlation = rho
+
+
+#' TODO: need to figure out how to do this for all Ct values 
+#' (not just first 10,000 rows)
+#' https://datasquad.at.sites.carleton.edu/data/storage-design/dealing-with-a-vector-memory-exhausted-error-in-r/
+#' https://nceas.github.io/oss-lessons/parallel-computing-in-r/parallel-computing-in-r.html
+
+# working on whole data set
+library(parallel)                          # base library for parallel computing
+# Import Ct value info
+probit_input <- read.csv("probit_zscores_cts_tissue_agnostic.csv")
+
+# limit to just ct_values and first 10,000 rows of data 
+# using more data produces "error: vector memory exhausted (limit reached)"
+probit_input <- as.data.frame(probit_input[ ,2])
+
+n <- nrow(probit_input)
+rho <- 0.2                                  # desired correlation (= cos(angle))
+theta <- acos(rho)                                         # corresponding angle
+cor_dat <- as.data.frame(rnorm(n, 1, 0.5))                         # new random data
+# should this be Weibull dis as well? which parameters to choose?
+X <- cbind(probit_input, cor_dat) # matrix
+Xctr <- scale(X, center = TRUE, scale = FALSE)     # centered columns (mean = 0)
+rm(X)                                       # remove unneeded data to save space
+
+fx <- function(Xctr){
+  n <- nrow(Xctr)
+  Id <- diag(n)  
+  Q <- qr.Q(qr(Xctr[ , 1, drop = FALSE]))     # QR decomp. of Xctr (original data)
+  P <- tcrossprod(Q)             # = Q Q'    # projection onto space defined by x1
+  x2o <- (Id-P) %*% Xctr[ , 2]                    # x2ctr made orthogonal to x1ctr 
+  Xc2 <- cbind(Xctr[ , 1], x2o)                                   # bind to matrix
+  Y <- Xc2 %*% diag(1/sqrt(colSums(Xc2^2)))            # scale columns to length 1
+  return(Y)
+}
+
+system.time(
+  results <- mclapply(Xctr, fx, mc.cores = detectCores()) # doesn't work :(
+)
+
+x <- Y[ , 2] + (1 / tan(theta)) * Y[ , 1]      # final vector of correlated data
+
+
+cor(probit_input, x)                                   # check correlation = rho
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
