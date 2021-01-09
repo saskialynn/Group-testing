b978af90c3e78c7577e0880e342ce2580c73f603
Saskia <saskia.comess@yale.edu>
2021-01-08 19:22:30 -0500

    comments added to MCMC.R

--- MCMC.R
+++ MCMC.R
@@ -1,3 +1,12 @@
+# nb_group <- 4
+# nb_out <- 5
+# N <- nb_group + nb_out
+# B <- 1000
+# Sigma <- matrix(runif(N^2, 0.1, 0.9), nrow = N, ncol = N) # generate random values from .1 to 0.9
+# Sigma = 0.5 * (Sigma + t(Sigma)) # make matrix symmetrical 
+# diag(Sigma)= rep(1,N) # 1's on diagonal 
+# prev <- .02*rep(1:(N))
+
 simulate_infections <- function(nb_group, nb_out, Sigma, prev, B=1000){
   #### 
   ## v is the vector of individual probabilities of becoming infected
@@ -6,9 +15,9 @@ simulate_infections <- function(nb_group, nb_out, Sigma, prev, B=1000){
   ## B is the number of simulations
   ####
   #print(index_group)
-  G = nb_group
+  G = nb_group # number of correlated individuals infected by network
   N = nb_group + nb_out 
-  P = nb_out
+  P = nb_out # number uncorrelated individuals infected by community
   infector <- numeric(B)
 
   if (G >=1){    #### if the group is not of size 0
@@ -17,30 +26,30 @@ simulate_infections <- function(nb_group, nb_out, Sigma, prev, B=1000){
     print(1:G)
     print(prev)
     print(v)
-    proba = 1-exp(sum(log(1 - prev[1:G]))) #### proba that one is the group is infected
+    proba = 1-exp(sum(log(1 - prev[1:G]))) #### proba that at least one is the group is infected
     #withProgress(message = paste0('Running ', B, ' simulations for pool size ', N), value = 0, {
     for (i in 1:B){
       # choose starting infected individual (assume that we know that there is at least one infection)
       # transmit infection
-      infector[i] <- which(rmultinom(1, size = 1, prob= (v/sum(v))) > 0) 
+      infector[i] <- which(rmultinom(1, size = 1, prob= (v/sum(v))) > 0) # choose initial infectious individual
       infected[infector[i], i] <- 1 # infector 
-      rest = setdiff(1:G, infector[i])
-      infected[rest, i] <- sapply(rest, function(x){rbinom(1,1,Sigma[infector[i], x])})
+      rest = setdiff(1:G, infector[i]) # susceptible individuals
+      infected[rest, i] <- sapply(rest, function(x){rbinom(1,1,Sigma[infector[i], x])}) # infections transmitted in network
       #incProgress(1/B, detail = paste("*"))
     }
     #})
-    temp <- apply(infected, 2, sum)
+    temp <- apply(infected, 2, sum) # column sum
     temp <- factor(temp, levels= 0:N)
     res_temp = as.data.frame(table(temp))
-    res_temp$Freq = res_temp$Freq/B * proba
-    res_temp$Freq[1] = 1 - proba
+    res_temp$Freq = res_temp$Freq/B * proba # weight by probability of somebody being infected
+    res_temp$Freq[1] = 1 - proba # probability of nobody being infected
     names(res_temp) = c("n", "Freq")
   }else{
     res_temp = data.frame(n = 0:N, Freq=c(1, rep(0,N)))
   }
   
   if (P >=1){
-    temp2 <- sapply(1:B,function(x){sum(sapply(prev[G+1:N], function(x) rbinom(1,1,x)))})
+    temp2 <- sapply(1:B,function(x){sum(sapply(prev[(G+1):N], function(x) rbinom(1,1,x)))}) # total number infections transmitted from community
     temp2 <- factor(temp2, levels= 0:(N))
     res_temp2 = as.data.frame(table(temp2))
     res_temp2$Freq = res_temp2$Freq/B
@@ -56,8 +65,8 @@ simulate_infections <- function(nb_group, nb_out, Sigma, prev, B=1000){
    #print(res_temp)
    #print(res_temp2)
    
-   for (l in 0:N){
-     for (k in 0:l){
+   for (l in 0:N){ # loop over all people in group (N total)
+     for (k in 0:l){ 
         res[l + 1, 2] = res[l + 1, 2] +  as.numeric(res_temp[which(res_temp$n == k), "Freq"]) * as.numeric(res_temp2[which(res_temp2$n == l- k), "Freq"])
       }
     }
